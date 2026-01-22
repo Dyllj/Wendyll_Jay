be7d9cc46d064a8fe05d9ecaa40e12f29109b2dc
Dyll <wtautoan73@gmail.com>
2026-01-22 15:31:03 +0800

    skill card

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import About from './pages/About'
 import Works from './pages/Work'
 import Contact from './pages/Contact'
 import SplitText from './components/SplitText/Split_Text'
-import AnimatedButton from './components/AnimationHover/Animation_Hover'
+import Button from './components/AnimationHover/Animation_Hover'
+import ScrollFadeIn from './components/ScrollFadeIn/Scroll_FadeIn'
 
 function App() {
   return (
     <div className="min-h-screen bg-zinc-800">
-      <header className="bg-zinc-200 shadow-lg w-full fixed">
+      <header className="bg-zinc-200 shadow-lg fixed w-full top-0 left-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
-            <div className="text-2xl ml-50 font-bold text-zinc-800">
+            <div className="text-2xl font-bold text-zinc-800">
               Portfolio
             </div>
-            
             <nav aria-label="Main navigation">
               <ul className="flex gap-15">
                 <li>
                   <a 
                     href="#about" 
-                    className="p-2 rounded-xl font-medium text-zinc-800 hover:bg-zinc-700 hover:text-zinc-200 transition-all ease-in-out"
+                    className="text-zinc-800 p-2 rounded-xl m-auto hover:bg-zinc-800 hover:text-zinc-100 transition-colors font-medium"
                   >
                     About Me
                   </a>
@@ -28,7 +28,7 @@ function App() {
                 <li>
                   <a 
                     href="#works" 
-                    className="p-2 rounded-xl font-medium text-zinc-800 hover:bg-zinc-700 hover:text-zinc-200 transition-all ease-in-out"
+                    className="text-zinc-800 p-2 rounded-xl m-auto hover:bg-zinc-800 hover:text-zinc-100 transition-colors font-medium"
                   >
                     Works
                   </a>
@@ -36,7 +36,7 @@ function App() {
                 <li>
                   <a 
                     href="#contact" 
-                    className="p-2 rounded-xl font-medium text-zinc-800 hover:bg-zinc-700 hover:text-zinc-200 transition-all ease-in-out"
+                    className="text-zinc-800 p-2 rounded-xl m-auto hover:bg-zinc-800 hover:text-zinc-100 transition-colors font-medium"
                   >
                     Contact
                   </a>
@@ -50,7 +50,7 @@ function App() {
       {/* Hero Section */}
       <article className="bg-linear-to-b from-zinc-200 to-zinc-800 min-h-screen flex items-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-          <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="flex flex-col md:flex-row items-center gap-10">
             <figure className="shrink-0">
               <img 
                 src="https://via.placeholder.com/300x300" 
@@ -62,9 +62,9 @@ function App() {
             <div className="flex-1 text-center md:text-left">
               <SplitText
                 text="HELLO I AM WENDYLL JAY TAUTOAN"
-                className="text-4xl md:text-5xl font-bold text-zinc-800 mb-4"
+                className="text-4xl md:text-5xl font-bold text-zinc-900 mb-4"
                 delay={50}
-                duration={0.5}
+                duration={1.25}
                 ease="power3.out"
                 splitType="chars"
                 from={{ opacity: 0, y: 40 }}
@@ -74,20 +74,23 @@ function App() {
                 textAlign="left"
                 tag="h1"
               />
-              <p className="text-lg text-zinc-800 ">
-                A developer with a passion for creating innovative and efficient solutions through code.
+              <ScrollFadeIn>
+              <p className="text-xl font-bold text-zinc-800">
+                A developer with a passion for creating innovative and efficient solutions through code. 
               </p>
-              <p className="text-lg text-zinc-800 mb-6">
+              <p className="text-xl font-bold text-zinc-800 mb-6">
                 Get to know me more!
               </p>
               <a href="#about">
-                <AnimatedButton />
+                <Button />
               </a>
+              </ScrollFadeIn>
             </div>
           </div>
         </div>
       </article>
-      <main className="">
+
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <About />
         <Works />
         <Contact />
