4a74af245451d794c875ab0a7ec0156bbb2b0e0e
Dyll <wtautoan73@gmail.com>
2026-01-22 16:04:11 +0800

    changes in skill card

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className="relative overflow-hidden w-60 h-80 rounded-3xl cursor-pointer text-2xl font-bold bg-purple-400">
-      <div className="z-10 absolute w-full h-full peer" />
-      <div className="absolute peer-hover:-top-20 peer-hover:-left-16 peer-hover:w-[140%] peer-hover:h-[140%] -top-32 -left-16 w-32 h-44 rounded-full bg-purple-300 transition-all duration-500" />
-      <div className="absolute flex text-xl text-center items-end justify-end peer-hover:right-0 peer-hover:rounded-b-none peer-hover:bottom-0 peer-hover:items-center peer-hover:justify-center peer-hover:w-full peer-hover:h-full -bottom-32 -right-16 w-36 h-44 rounded-full bg-purple-300 transition-all duration-500">
-        Nice to meet u,<br />Uiverse
+    <div className="group flex flex-col justify-start items-start gap-2 w-96 h-56 duration-500 relative rounded-lg p-4 bg-gray-100 hover:-translate-y-2 hover:shadow-xl shadow-gray-300">
+      <div className="absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-10 -right-10 w-1/2 h-1/2 rounded-lg bg-gray-200" alt="image here" />
+      <div className>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">Card</h2>
+        <p className="text-gray-700 line-clamp-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis
+          magna quis lectus fermentum, quis scelerisque orci pellentesque. Duis id
+          porta justo. Sed ac enim id justo tincidunt hendrerit id ac lectus.
+          Pellentesque maximus posuere tortor vitae consequat.
+        </p>
       </div>
-      <div className="w-full h-full items-center justify-center flex uppercase">
-        hover me
-      </div>
+      <button className="hover:bg-gray-300 bg-gray-200 text-gray-800 mt-6 rounded p-2 px-6">
+        Explore
+      </button>
     </div>
   );
 }
